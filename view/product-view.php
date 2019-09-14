626bb1c478174d73fe72c48b7017f26b33f10c28
Mr. Do <bemr.do@gmail.com>
2019-09-14 14:57:58 +0800

    product view done - make zoom view

--- view/product-view.php
+++ view/product-view.php
@@ -8,16 +8,14 @@
 <style>
     .swiper-container {
         width: 100%;
-        height: 100%;
     }
 
     .swiper-slide {
-        height: 400px;
+        height: 110px;
         text-align: center;
         vertical-align: middle;
         font-size: 18px;
         padding: 0 auto;
-        background: #d8d8d8;
         /* Center slide text vertically */
         display: -webkit-box;
         display: -ms-flexbox;
@@ -33,35 +31,14 @@
         align-items: center;
     }
 
-    .swiper-section {
-        padding: 4rem 0 1rem 0;
-    }
-
-    .swiper-pagination-bullet-active,
-    .swiper-pagination-bullet:hover {
-        vertical-align: middle;
-        width: 12px;
-        height: 12px;
-        transition: 0.2s;
-    }
-
     .swiper-button-next, .swiper-button-prev {
-        top: 50%;
-        transform: translateY(-30%);
+        transform: translateY(20%);
         background-color: hsl(48, 100%, 67%);
         color: rgba(0, 0, 0, 0.7);
-        height: 50px;
-        width: 50px;
+        height: 30px;
+        width: 30px;
         border-radius: 50%;
         background-image: none;
-    }
-
-    .swiper-button-next {
-        right: 30px;
-    }
-
-    .swiper-button-prev {
-        left: 30px;
     }
 
     .fa-chevron-right, .fa-chevron-left {
@@ -79,6 +56,13 @@
         padding-right: 5px;
     }
 
+    .swiper-button-next {
+        right: 0px;
+    }
+
+    .swiper-button-prev {
+        left: 0px;
+    }
 </style>
 
 <section class="section">
@@ -92,42 +76,43 @@
         </div>
         <div class="columns">
             <div class="column is-one-third product-image">
-
                 <div class="main-image">
                     <img src="../image/product/pkg1.jpg" id="currentImg" alt="Image Product" title="pkg1">
                 </div>
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide product-thumb-image">
-                        <img src="../image/product/pkg1.jpg" onclick="showImage('../image/product/pkg1.jpg','pkg1');" data-pin-nopin="true">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide product-thumb-image">
+                            <img src="../image/product/pkg1.jpg" onclick="showImage('../image/product/pkg1.jpg','pkg1');" data-pin-nopin="true">
+                        </div>
+                        <div class="swiper-slide product-thumb-image">
+                            <img src="../image/product/pkg2.jpg" onclick="showImage('../image/product/pkg2.jpg','pkg2');" data-pin-nopin="true">
+                        </div>
+                        <div class="swiper-slide product-thumb-image">
+                            <img src="../image/product/pkg3.jpg" onclick="showImage('../image/product/pkg3.jpg','pkg3');" data-pin-nopin="true">
+                        </div>
+                        <div class="swiper-slide product-thumb-image">
+                            <img src="../image/product/pkg4.jpg" onclick="showImage('../image/product/pkg4.jpg','pkg4');" data-pin-nopin="true">
+                        </div>
+                        <div class="swiper-slide product-thumb-image">
+                            <img src="../image/product/pkg5.jpg" onclick="showImage('../image/product/pkg5.jpg','pkg5');" data-pin-nopin="true">
+                        </div>
                     </div>
-                    <div class="swiper-slide product-thumb-image">
-                        <img src="../image/product/pkg2.jpg" onclick="showImage('../image/product/pkg2.jpg','pkg2');" data-pin-nopin="true">
-                    </div>
-                    <div class="swiper-slide product-thumb-image">
-                        <img src="../image/product/pkg3.jpg" onclick="showImage('../image/product/pkg3.jpg','pkg3');" data-pin-nopin="true">
-                    </div>
-                    <div class="swiper-slide product-thumb-image">
-                        <img src="../image/product/pkg4.jpg" onclick="showImage('../image/product/pkg4.jpg','pkg4');" data-pin-nopin="true">
-                    </div>
-                    <div class="swiper-slide product-thumb-image">
-                        <img src="../image/product/pkg5.jpg" onclick="showImage('../image/product/pkg5.jpg','pkg5');" data-pin-nopin="true">
-                    </div>
-                </div>
-                <!-- Add Arrows -->
-                <div class="swiper-button-next is-hidden-mobile">
-                    <i class="fas fa-chevron-right fa-2x"></i>
-                </div>
-                <div class="swiper-button-prev is-hidden-mobile">
-                    <i class="fas fa-chevron-left fa-2x"></i>
-                </div>
 
+                    <!-- Add Arrows -->
+                    <div class="swiper-button-next is-hidden-mobile is-pulled-right">
+                        <i class="fas fa-chevron-right fa-lg"></i>
+                    </div>
+                    <div class="swiper-button-prev is-hidden-mobile">
+                        <i class="fas fa-chevron-left fa-lg"></i>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </section>
 
 <!-- Swiper JS -->
-<script src="../dist/js/swiper.min.js"></script>
+<script src="../js/swiper.min.js"></script>
 
 <script type="text/javascript">
 
@@ -140,12 +125,12 @@
     }
 
     var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
+        spaceBetween: 4,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
+        slidesPerView: 4.5,
     });
 </script>
 
