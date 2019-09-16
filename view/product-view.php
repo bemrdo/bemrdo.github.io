d9fa4b6c3cd48dcfcc410b0794568941e2d4948a
Mr. Do <bemr.do@gmail.com>
2019-09-16 17:10:01 +0800

    detail product share link done with sweetalert2 - make zoom view, detail data

--- view/product-view.php
+++ view/product-view.php
@@ -2,67 +2,12 @@
 <?php include("navbar.php") ?>
 
 <link rel="stylesheet" href="../css/product.css">
-<link rel="stylesheet" href="../css/product-view.css">
 <link rel="stylesheet" href="../css/swiper.min.css">
+<link rel="stylesheet" href="../css/product-view.css">
+<link rel="stylesheet" href="../css/bulma-tooltip.min.css">
 
 <style>
-    .swiper-container {
-        width: 100%;
-    }
 
-    .swiper-slide {
-        height: 110px;
-        text-align: center;
-        vertical-align: middle;
-        font-size: 18px;
-        padding: 0 auto;
-        /* Center slide text vertically */
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
-        -webkit-box-pack: center;
-        -ms-flex-pack: center;
-        -webkit-justify-content: center;
-        justify-content: center;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        -webkit-align-items: center;
-        align-items: center;
-    }
-
-    .swiper-button-next, .swiper-button-prev {
-        transform: translateY(20%);
-        background-color: hsl(48, 100%, 67%);
-        color: rgba(0, 0, 0, 0.7);
-        height: 30px;
-        width: 30px;
-        border-radius: 50%;
-        background-image: none;
-    }
-
-    .fa-chevron-right, .fa-chevron-left {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-
-    .fa-chevron-right {
-        padding-left: 5px;
-    }
-
-    .fa-chevron-left {
-        padding-right: 5px;
-    }
-
-    .swiper-button-next {
-        right: 0px;
-    }
-
-    .swiper-button-prev {
-        left: 0px;
-    }
 </style>
 
 <section class="section">
@@ -81,24 +26,25 @@
                 </div>
                 <div class="swiper-container">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide product-thumb-image">
+                        <div class="swiper-slide">
                             <img src="../image/product/pkg1.jpg" onclick="showImage('../image/product/pkg1.jpg','pkg1');" data-pin-nopin="true">
                         </div>
-                        <div class="swiper-slide product-thumb-image">
+                        <div class="swiper-slide">
                             <img src="../image/product/pkg2.jpg" onclick="showImage('../image/product/pkg2.jpg','pkg2');" data-pin-nopin="true">
                         </div>
-                        <div class="swiper-slide product-thumb-image">
+                        <div class="swiper-slide">
                             <img src="../image/product/pkg3.jpg" onclick="showImage('../image/product/pkg3.jpg','pkg3');" data-pin-nopin="true">
                         </div>
-                        <div class="swiper-slide product-thumb-image">
+                        <div class="swiper-slide">
                             <img src="../image/product/pkg4.jpg" onclick="showImage('../image/product/pkg4.jpg','pkg4');" data-pin-nopin="true">
                         </div>
-                        <div class="swiper-slide product-thumb-image">
+                        <div class="swiper-slide">
                             <img src="../image/product/pkg5.jpg" onclick="showImage('../image/product/pkg5.jpg','pkg5');" data-pin-nopin="true">
                         </div>
                     </div>
-
-                    <!-- Add Arrows -->
+                </div>
+                <!-- Add Arrows -->
+                <div class="arrow-container">
                     <div class="swiper-button-next is-hidden-mobile is-pulled-right">
                         <i class="fas fa-chevron-right fa-lg"></i>
                     </div>
@@ -107,15 +53,57 @@
                     </div>
                 </div>
             </div>
+            <div class="column is-two-fifths product-order">
+                <div class="title is-size-3">
+                    <p>Nama Product panjang disini panjang sekali</p>
+                </div>
+                <div class="is-size-6 product-stock is-paddingless">
+                    <p>
+                        <strong>88</strong> Terjual &nbsp; &#124; &nbsp;<strong>3</strong> Terpesan dari <strong>4</strong> Tersedia &nbsp;<span class="has-text-centered tooltip is-tooltip-warning is-tooltip-right is-tooltip-left-mobile is-tooltip-multiline" data-tooltip="Terjual adalah jumlah barang yang telah sukses terjual. Terpesan adalah jumlah barang yang saat ini terpesan (batas 24 jam sejak dipesan) dari stok barang yang Tersedia saat ini."><i class="fa fa-info-circle"></i></span>&nbsp;
+                        <span>
+                            <a class="button is-small is-info is-outlined btn-copy is-pulled-right" onclick="copyTC('#url')">
+                                <span class="icon is-small">
+                                    <i class="fas fa-link"></i>
+                                </span>
+                                <span>Share</span>
+                            </a>
+                            <p id="url" style="display:none;">P2: I am a second paragraph</p>
+                        </span>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
+
+<!-- SweetAlert2 -->
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8.17.1/dist/sweetalert2.all.min.js"></script>
+
+<!-- Copy Link -->
+<script type="text/javascript">
+    function copyTC(element) {
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(element).text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+        Swal.fire({
+            //position: 'top-end',
+            type: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1000
+        })
+    }
+
+    document.getElementById("url").innerHTML = window.location.href;
+</script>
 
 <!-- Swiper JS -->
 <script src="../js/swiper.min.js"></script>
 
 <script type="text/javascript">
-
     function showImage(imgPath, imgText) {
         var curImage = document.getElementById('currentImg');
 
@@ -132,6 +120,7 @@
         },
         slidesPerView: 4.5,
     });
+
 </script>
 
 <?php include("footer.php") ?>
